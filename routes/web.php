d67a1df80a42b589f95ee6af7439ebfdd41bfde2
behike56 <kc7u9k9tim3jfqtrrn3jykgecdwurk@gmail.com>
2019-03-01 10:10:12 +0900

    add

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+     Route::get('news/delete', 'Admin\NewsController@delete');
+     Route::get('news/update', 'Admin\NewsController@update');
+
+     //10-3
+     Route::get('/XXX', 'Admin\AAAController@bbb');
+
+     //10-4,13-2
+     Route::get('profile/create', 'Admin\ProfileController@add'->middleware('auth'));
+     Route::get('profile/edit', 'Admin\ProfileController@edit'->middleware('auth'));
+
+
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
