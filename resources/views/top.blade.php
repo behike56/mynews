10c66f5562ade81e25ffaa83d11351d673715cf4
behike56 <kc7u9k9tim3jfqtrrn3jykgecdwurk@gmail.com>
2019-03-28 16:36:18 +0900

    Update: add top-page

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -64,34 +64,36 @@
         </style>
     </head>
     <body>
-	<div class="container">
-	    <div class="row justify-content-center">
-		<div class="col-md-8">
-		    <div class="card">
-			<div class="card-header">TOP page</div>
-			<div class="card-body" >
-			    Welcome.
-			</div>
-			<div class="card-header">
-			    <a href="{{url('/index')}}">
-				<p>ニュース記事を見る</p>
-			    </a>
-			    <a href="{{url('/profile')}}">
-				<p>投稿者プロフィールを見る</p>
-			    </a>
-			</div>
-			<div class="card-header">投稿者用ページ</div>
-			<div class="card-header">
-			    <a href="{{url('/news/create')}}">
-				<p>記事を投稿する</p>
-			    </a>
-			    <a href="{{url('/profile/create')}}">
-				<p>プロフィール追加＆編集</p>
-			    </a>
-			</div>
-		    </div>
-		</div>
-	    </div>
-	</div>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                    <a href="{{ url('/home') }}">Home</a>
+            @else
+                    <a href="{{ route('login') }}">Login</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
